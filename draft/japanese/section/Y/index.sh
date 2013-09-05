1b6b43b7a98e632b3359d8e10ae84f07b0ad5089
seinokatsuhiro <seinokatsuhiro@me.com>
2013-09-06 03:45:27 +0900

    演算子索引 - 索引生成プログラムを追加

---
+++ draft/japanese/section/Y/index.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+#  DESCRIPTION
+#    Generate operator index
+#
+#  USAGE
+#    ./index.sh
+#
+
+github=https://github.com/seinokatsuhiro/abc-of-koshucode
+alpha=A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z
+
+stderr () {
+    echo "$*" 1>&2
+}
+
+gen_link () {
+    stderr "Generate links in markdown"
+    for a in `echo $alpha | tr - ' '` ; do
+        echo "[$a]: $github/blob/master/draft/japanese/section/$a"
+    done
+}
+
+gen_koshu_element () {
+    stderr "Generate judges of KOSHU-RELMAP-ROP"
+    (cd .. ; koshu --element [A-Z]/*.k)
+}
+
+gen_rop_index () {
+    stderr "Generate judges of INDEX"
+    koshu Y.k $1
+}
+
+gen_rop_table () {
+    stderr "Generate index table in markdown"
+    runhaskell table.hs INDEX /order /rop /sects $alpha < $1
+}
+
+mkdir -p output
+
+gen_link                       > output/LINK.md
+gen_koshu_element              > output/ELEMENT.k
+gen_rop_index output/ELEMENT.k > output/INDEX.k
+gen_rop_table output/INDEX.k   > output/TABLE.md
+
---
+++ draft/japanese/section/Y/table.hs
@@ -0,0 +1,81 @@
+#!/usr/bin/env runhaskell
+{-# OPTIONS_GHC -Wall #-}
+-- ------------------------------------------------------------
+--
+--  DESCRIPTION
+--    Generate index table in markdown.
+--    
+--    Input is in koshucode
+--      |-- INDEX  /order text
+--                 /word  text
+--                 /sects [ text ]
+--    
+--    Output is in markdown table
+--      | order | word | [`A`][A] ... |
+--
+--  USAGE
+--    ./table.hs INDEX /order /word /sects A-B-C-D-E-F-G
+--
+-- ------------------------------------------------------------
+
+import qualified Data.List                as List
+import qualified Data.Maybe               as Maybe
+import qualified System.Environment       as Sys
+import qualified Text.PrettyPrint         as Pretty
+import qualified Koshucode.Baala.Base     as B
+import qualified Koshucode.Baala.Core     as C
+import qualified Koshucode.Baala.Vanilla  as V
+
+data Param = Param
+    { paramPattern :: String
+    , paramOrder   :: String
+    , paramLine    :: String
+    , paramColumn  :: String
+    , paramAlpha   :: String
+    } deriving (Show)
+
+paramPages :: Param -> [String]
+paramPages Param { paramAlpha = alpha } = B.divide '-' alpha
+
+main :: IO ()
+main =
+    do [pat, order, line, column, alpha] <- Sys.getArgs
+       let param = Param pat order line column alpha
+           root  = C.emptySection :: C.Section V.VContent
+       code <- getContents
+       case C.sectionRead root "stdin" code of
+         Right sec -> putLines $ indexLines param $ C.sectionJudge sec
+         Left a    -> B.abort a
+
+putLines :: [String] -> IO ()
+putLines = putStr . unlines
+
+indexLines :: (C.CContent c) => Param -> [B.Judge c] -> [String]
+indexLines param = List.sort . map (indexLine param)
+
+indexLine :: (C.CContent c) => Param -> B.Judge c -> String
+indexLine param (B.Judge True p xs)
+    | p == paramPattern param
+        = show $ B.doch line where
+    the n = Maybe.fromJust $ lookup n xs
+    order = C.getText $ the (paramOrder param)
+    word  = C.getText $ the (paramLine  param)
+    sects = List.sort $ getTextList $ the (paramColumn param)
+    bar   = B.doc "|"
+    line  = [ bar, B.doc (B.padRight 28 order)
+            , bar, B.doc word
+            , bar, Pretty.hcat $ map B.doc $ linkCode param sects
+            , bar ]
+indexLine p j = error $ show (p, j)
+
+getTextList :: (C.CText c, C.CList c) => c -> [String]
+getTextList = map C.getText . C.getList
+
+linkCode :: Param -> B.Map [String]
+linkCode param files = map link $ sourceList param where
+    link x | x `elem` files = "[`" ++ x ++ "`][" ++ x ++ "]"
+           | otherwise      = "`-`"
+
+sourceList :: Param -> [String]
+sourceList param = filter (/= "-") $ paramPages param
+
