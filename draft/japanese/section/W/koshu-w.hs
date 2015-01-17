a70a9fc4755f5c1e1004d94f360d071357b08eec
SEINO Katsuhiro <seinokatsuhiro@me.com>
2015-01-17 22:14:43 +0900

    甲州計算機の拡張 - ライブラリにあわせて修正

--- draft/japanese/section/W/koshu-w.hs
+++ draft/japanese/section/W/koshu-w.hs
@@ -15,8 +15,10 @@ module Main (main) where
 
 import qualified Koshucode.Baala.Base                   as B
 import qualified Koshucode.Baala.Core                   as C
+import qualified Koshucode.Baala.Op.Global              as Op
 import qualified Koshucode.Baala.Op                     as Op
-import qualified Koshucode.Baala.Op.Message             as Message
+import qualified Koshucode.Baala.Type.Vanilla           as Type
+import qualified Koshucode.Baala.Op.Message             as Msg
 import qualified Koshucode.Baala.Toolkit.Main.KoshuMain as Main
 import qualified Koshucode.Baala.Toolkit.Library.Exit   as Main
 import qualified Paths_koshu_w                          as Paths
@@ -25,16 +27,17 @@ import qualified Paths_koshu_w                          as Paths
 -- ----------------------  main
 
 main :: IO ()
-main = Main.exit =<< Main.koshuMain g where
-    rops = C.globalRops Op.vanillaGlobal
-    g = Op.vanillaGlobal { C.globalVersion = Paths.version
-                          , C.globalRops    = userRops ++ rops }
+main = Main.exit =<< Main.koshuMain koshuGlobal where
+
+koshuGlobal :: C.Global Type.VContent
+koshuGlobal = C.globalRopsAdd g' userRops where
+    g' = Op.vanillaGlobal { C.globalSynopsis = "The ABC of Koshucode"
+                          , C.globalVersion  = Paths.version }
 
 -- User-defined relmap operator
-userRops :: [C.Rop Op.VContent]
+userRops :: [C.Rop Type.VContent]
 userRops = Op.ropList "user"
-    [ ( "divide /T /T /T /T", consDivide,
-        C.sortEnum ["-1", "-2", "-3", "-4"] [] ) ]
+    [ Op.ropE consDivide "divide /T /T /T /T"  "-1 -2 -3 -4" ]
 
 
 -- ----------------------  divide
@@ -60,19 +63,19 @@ consDivide use =
      r <- Op.getTerm use "-4"
      Right $ C.relmapFlow use $ relkitDivide [x, y, q, r]
 
-relkitDivide :: (C.CDec c) => [B.TermName] -> C.RelkitCalc c
+relkitDivide :: (C.CDec c) => [B.TermName] -> C.RelkitFlow c
 relkitDivide ns@[_, _, q, r] he1'@(Just he1)
     | B.operand [xi, yi] [qi, ri] = relkitDivideQR (xi, yi, q, r) he1'
-    | otherwise = Message.unkTerm ns he1
+    | otherwise = Msg.unkTerm ns he1
     where
       [xi, yi, qi, ri] = ns `B.snipFull` B.headNames he1
 relkitDivide _ _ = Right C.relkitNothing
 
 relkitDivideQR :: forall c. (C.CDec c)
-    => (Int, Int, B.TermName, B.TermName) -> C.RelkitCalc c
+    => (Int, Int, B.TermName, B.TermName) -> C.RelkitFlow c
 relkitDivideQR _ Nothing = Right C.relkitNothing
 relkitDivideQR (xi, yi, q, r) (Just he1) = Right kit2 where
-    he2    = B.headCons2 (q, r) he1
+    he2    = B.headAppend [q, r] he1
     kit2   = C.relkitJust he2 $ C.RelkitOneToAbMany False kitf2 []
     kitf2 _ cs1 =
         do let [xc, yc] = B.snipFrom [xi, yi] cs1
